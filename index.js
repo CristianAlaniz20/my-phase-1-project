6313a96478885e79b87bc4f11cc18a9ceff9c75d
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-01-13 12:47:56 -0800

    add team-container div in index.html, make team div and sub elements in index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", e => {
     const h2 = document.createElement("h2");
-    h2.textContent = "This content added by JavaScript";
+    h2.textContent = "As of January 10th, 2024";
     document.querySelector("body").appendChild(h2);
 
     createLeagueDiv()
+    createTeamDiv()
 })
 
 //creates league divs and fetches their info
@@ -30,19 +31,56 @@ function createLeagueDivSubElements(div) {
     h2.classList.add("league-name")
     h2.textContent = this.name
     */
-    const img = document.createElement("img")
-    img.setAttribute("src", this.logo)
-    img.classList.add("league-img")
+    const leagueLogoImg = document.createElement("img")
+    leagueLogoImg.setAttribute("src", this.logo)
+    leagueLogoImg.classList.add("league-img")
 
-    const h4 = document.createElement("h4")
-    h4.classList.add("league-country")
-    h4.setAttribute("id", this.country)
-    h4.textContent = this.country
+    const countryTextElement = document.createElement("h3")
+    countryTextElement.classList.add("league-country")
+    countryTextElement.setAttribute("id", this.country)
+    countryTextElement.textContent = this.country
 
     
     //const subElementsArray = [h2, img, h4]
     //subElementsArray.forEach(element => div.appendChild(element))
-    const subElementsArray = [h4, img]
+    const subElementsArray = [countryTextElement, leagueLogoImg]
     subElementsArray.forEach(element => div.appendChild(element))
     //div.appendChild(img)
 }
+
+function createTeamDiv() {
+    fetch("http://127.0.0.1:3000/leagues")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            const teamDivContainer = document.getElementById("team-container")
+            const teamsArray = element.top3Teams;
+            const teamDiv = document.createElement("div")
+            teamDiv.classList.add("team-div")
+            iterateThroughTeamsArray(teamsArray, teamDiv)
+            teamDivContainer.appendChild(teamDiv)
+        })
+    })
+}
+
+function iterateThroughTeamsArray(array, div) {
+    array.forEach(teamObj => {
+        const teamName = document.createElement("h4")
+        teamName.classList.add("team-name")
+        teamName.textContent = teamObj.team
+        const teamBadge = document.createElement("img")
+        teamBadge.setAttribute("src", teamObj.badge)
+        teamBadge.classList.add("team-badge")
+        const foundedYear = document.createElement("p")
+        foundedYear.classList.add("founded-year")
+        foundedYear.textContent = teamObj.founded
+        
+        const teamSubElementsArray = [teamName, teamBadge, foundedYear]
+        teamSubElementsArray.forEach(element => div.appendChild(element))
+    })
+}
+
+//assign an element to top3teams of each element
+//iterate through top3teams array (forEach)
+//make a div with with an id and class for each item
+//make subelements for the div made
