22eb5716120b8d4b15d81dc32d7d8b75846aa619
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-01-17 17:14:17 -0800

    added ball div and button element to index.html. Gave the ball div css style to look like a soccer ball. Added the code to move the ball left and right in index.js. Also removed some code that was not being used.

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ document.addEventListener("DOMContentLoaded", e => {
     createLeagueDiv()
     addDropdownEventListener()
     fetchDropdownOptionData()
-
+    moveBall()
 })
-
-let callCount = 0;
 
 //creates league divs and fetches their info
 function createLeagueDiv() {
@@ -18,9 +16,8 @@ function createLeagueDiv() {
     .then(data => {
         const leagueContainer = document.getElementById("league-container");
         data.forEach(element => {
-            //create div and set class and id name
+            //create league div and set class and id name
             const leagueDiv = document.createElement("div")
-            leagueDiv.setAttribute("id", element.id)
             leagueDiv.classList.add("league-div")
             //call function and append div to another div
             createLeagueDivSubElements.call(element, leagueDiv)
@@ -32,48 +29,15 @@ function createLeagueDiv() {
 
 //creates league div sub elements
 function createLeagueDivSubElements(div) {
-    //create image element and set its source and class name
-    const leagueLogoImg = document.createElement("img")
-    leagueLogoImg.setAttribute("src", this.logo)
-    leagueLogoImg.classList.add("league-img")
-    //create h3 element and set its class and id name
+    //create image element
+    const leagueLogoImg = createImage("league-img", this.logo)
+    //create h3 element and set its class, id, and text content
     const countryH3Element = document.createElement("h3")
     countryH3Element.classList.add("league-country")
-    countryH3Element.setAttribute("id", this.country)
     countryH3Element.textContent = this.country
-    //create element array and appendChild each one to passed in div
+    //create elements array and appendChild each one to passed in div
     const subElementsArray = [countryH3Element, leagueLogoImg]
     subElementsArray.forEach(element => div.appendChild(element))
-    
-    //div.appendChild(leagueLogoImg)
-}
-
-//Creates a player card for each element in the array, and appendChilds them to a div
-function createPlayerCards(array, div) {
-    array.forEach(professional => {
-        const professionalDiv = document.createElement("div")
-        professionalDiv.classList.add("player")
-        for (const info in professional) {
-            //adds info to card WITHOUT property text
-            if (info === "name") {
-                const playerInfo = document.createElement("p")
-                playerInfo.textContent = `${professional[info]}`;
-                professionalDiv.appendChild(playerInfo)
-            //create img tag for player pic and adds to card
-            } else if (info === "picture") {
-                const playerPic = document.createElement("img")
-                playerPic.classList.add("player-pic")
-                playerPic.setAttribute("src", professional[info])
-                professionalDiv.appendChild(playerPic)
-            //adds info to card WITH property text
-            } else {
-                const playerInfo = document.createElement("p")
-                playerInfo.textContent = `${info.toUpperCase()}: ${professional[info]}`;
-                professionalDiv.appendChild(playerInfo)
-            }
-        }
-        div.appendChild(professionalDiv)
-    })
 }
 
 //adds "change" event listener to league-dropdown
@@ -108,6 +72,13 @@ function addDropdownEventListener() {
     })
 }
 
+//removes all subelements of table and stats divs
+function removeTableAndPlayers() {
+    const tableDiv = document.getElementById("league-table")
+    tableDiv.removeChild(document.getElementById("table"))
+    Array.from(document.getElementsByClassName("player")).forEach(element => element.remove())
+}
+
 //fetch corresponding data to dropdown option
 function fetchDropdownOptionData(dropdownValue = 1) {
     fetch(`http://127.0.0.1:3000/leagues/${dropdownValue}`)
@@ -119,6 +90,32 @@ function fetchDropdownOptionData(dropdownValue = 1) {
             createPlayerCards(data.top3Assisters, document.getElementById("assists"))
         })
         .catch(error => console.log(error))
+}
+
+//Creates a player card for each element in the array, and appendChilds them to a div
+function createPlayerCards(array, div) {
+    array.forEach(professional => {
+        const professionalDiv = document.createElement("div")
+        professionalDiv.classList.add("player")
+        for (const info in professional) {
+            //adds info to card WITHOUT property text
+            if (info === "name") {
+                const playerInfo = document.createElement("p")
+                playerInfo.textContent = `${professional[info]}`;
+                professionalDiv.appendChild(playerInfo)
+            //create img tag for player pic and adds to card
+            } else if (info === "picture") {
+                const playerPic = createImage("player-pic", professional[info])
+                professionalDiv.appendChild(playerPic)
+            //adds info to card WITH property text
+            } else {
+                const playerInfo = document.createElement("p")
+                playerInfo.textContent = `${info.toUpperCase()}: ${professional[info]}`;
+                professionalDiv.appendChild(playerInfo)
+            }
+        }
+        div.appendChild(professionalDiv)
+    })
 }
 
 //creates initial table for a league
@@ -140,22 +137,13 @@ function createTable(leagueName) {
     tableHeaders.forEach(element => {
         //creates header badge cell
         if (tableHeaders[0] === element) {
-            const tableCell = document.createElement("td")
-            tableCell.classList.add("team-badge-td")
-            tableCell.textContent = element
-            tableRow.appendChild(tableCell)
+            tableRow.appendChild(createTableCell("team-badge-td", element))
         //creates header team name cell
         } else if (tableHeaders[1] === element) {
-            const tableCell = document.createElement("td")
-            tableCell.classList.add("team-name")
-            tableCell.textContent = element
-            tableRow.appendChild(tableCell)
+            tableRow.appendChild(createTableCell("team-name", element))
         //creates header stats cells 
         } else {
-            const tableCell = document.createElement("td")
-            tableCell.classList.add("table-data")
-            tableCell.textContent = element
-            tableRow.appendChild(tableCell)
+            tableRow.appendChild(createTableCell("table-data", element))
         }
     })
     table.appendChild(tableRow)
@@ -169,33 +157,87 @@ function createTableRows(array) {
         for(const info in team) {
             //creates badge cell
             if (info === "badge") {
-                const tableCell = document.createElement("td")
-                tableCell.classList.add("team-badge-td")
-                const teamBadge = document.createElement("img")
-                teamBadge.classList.add("team-badge")
-                teamBadge.setAttribute("src", team[info])
+                const tableCell = createTableCell("team-badge-td")
+                const teamBadge = createImage("team-badge", team[info])
                 tableCell.appendChild(teamBadge)
                 tableRow.appendChild(tableCell)
             //creates team name cell
             } else if (info === "team") {
-            const tableCell = document.createElement("td")
-            tableCell.classList.add("team-name")
-            tableCell.textContent = team[info]
-            tableRow.appendChild(tableCell)
+                tableRow.appendChild(createTableCell("team-name", team[info]))
             //creates table stats cells
             } else {
-                const tableCell = document.createElement("td")
-                tableCell.classList.add("table-data")
-                tableCell.textContent = team[info]
-                tableRow.appendChild(tableCell)
+                tableRow.appendChild(createTableCell("table-data", team[info]))
             }
         }
         table.appendChild(tableRow)
     })
 }
 
-function removeTableAndPlayers() {
-    const tableDiv = document.getElementById("league-table")
-    tableDiv.removeChild(document.getElementById("table"))
-    Array.from(document.getElementsByClassName("player")).forEach(element => element.remove())
+//creates image element, sets its class and source.
+function createImage(name, src) {
+    const image = document.createElement("img")
+    image.classList.add(name)
+    image.setAttribute("src", src)
+    return image
 }
+
+//creates td element, sets its class and text content
+function createTableCell(name, value) {
+    const tableCell = document.createElement("td")
+    tableCell.classList.add(name)
+    tableCell.textContent = value
+    return tableCell
+}
+
+let intervalId;
+let clickCount = 0;
+
+function moveBall() {
+    const button = document.getElementById("move-ball")
+    button.addEventListener("click", e => {
+        const ball = document.getElementById("ball")
+        let pos = 0;
+        clearInterval(intervalId)
+        if (clickCount % 2 === 0) {
+            intervalId = setInterval(() => {
+                if (pos === 650) {
+                    clearInterval(intervalId)
+                } else  {
+                    pos++;
+                    ball.style.left = pos + `px`;
+                }
+            })            
+            clickCount = 1;
+        } else {
+            pos = 650
+            intervalId = setInterval(() => {
+                if (pos === 0) {
+                    clearInterval(intervalId)
+                } else  {
+                    pos--;
+                    ball.style.left = pos + `px`;
+                }
+            })
+            clickCount = 0;
+        }
+    });
+};
+
+//["FC Barcelona", "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png"]
+//["Poland", "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Flag_of_Poland.svg/1200px-Flag_of_Poland.svg.png"]
+//adds info to card WITH property text
+            /*} else if (info === "club" || info === "nationality") {
+                const playerInfo = document.createElement("p")
+                professional[info].forEach(element => {
+                    if (element === professional[info][0]) {
+                        const clubName = element
+                        playerInfo.textContent = `${info.toUpperCase()}: ${clubName}`
+                        debugger
+                    } else {
+                        const image = createImage("player-personal-info-image", element)
+                        playerInfo.appendChild(image)
+                        debugger
+                    }
+                })
+                professionalDiv.appendChild(playerInfo)
+                debugger*/
